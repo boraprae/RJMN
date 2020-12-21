f6b9fccd89e0239466eb1777062ce110960fb184
prinnada <6231302027@lamduan.mfu.ac.th>
2020-12-21 14:07:59 +0700

    login (not with remember me)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
@@ -7,6 +8,9 @@ const bcrypt = require("bcrypt");
 const app = express();
 const con = mysql.createConnection(config);
  
+const e = require("express");
+const { database, password } = require("./dbConfig.js");
+
 //set "public" folder to be static folder, user can access it directly
 app.use(express.static(path.join(__dirname, "js")))
 app.use(express.static(path.join(__dirname, "css")))
@@ -20,56 +24,86 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(bodyParser.json()); 
  
 // ============= Create hashed password ==============
-app.get("/password/:pass", function (req, res) {
-    const password = req.params.pass;
-    const saltRounds = 10;    //the cost of encrypting see https://github.com/kelektiv/node.bcrypt.js#a-note-on-rounds
-    bcrypt.hash(password, saltRounds, function(err, hash) {
-        //return hashed password, 60 characters
-        // console.log(hash.length);
-        res.send(hash);
-    });
-});
+
  
 // ============= Login ==============
 app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
-    
- 
-    const sql = `SELECT username, id FROM user WHERE username ='${username}' AND password='${password}'`;
-    ;
+    const sql = "SELECT username FROM user WHERE USERNAME=? AND PASSWORD=?";
+
     con.query(sql, [username, password], function (err, result, fields) {
         if (err) {
             console.log(err);
-            res.status(500).send("Server error");
+            res.status(500).end("Server error");
             return;
         }
+
+        // result is an array of records from database
+        const numrows = result.length;
+        //if no data
+        if (numrows != 1) {
+            res.status(401).end("Wrong username or password");
+        }
         else {
-            const numrows = result.length;
-            //if that user is not unique
-            if (numrows != 1) {
-                //login failed
-                res.status(401).end("Wrong username or password");
+            if (result[0].username == 'Orawan021') {
+                res.send("/");
+                console.log("Success")
             }
             else {
                 res.send("/");
-
-                // console.log(result[0].password);
-                //verify password, async method
-                bcrypt.compare(password, result[0].password, function (err, resp) {
-                    if (err) {
-                        res.status(503).send("Authentication Server error");
-                    }
-                    else if (resp == true) {
-                        //correct login send destination URL to client
-                        res.send("e");
-                    }
-                    else {
-                        //wrong password
-                        res.status(401).send("Wrong password");
-                    }
-                });
             }
+        }
+    });
+});
+
+
+ //---- register ----
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const email = req.body.email;
+    const gender = req.body.gender;
+    const password = req.body.password;
+
+    bcrypt.hash(password, 10, function (err, hash) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Hash error");
+        } else {
+            //get hashed password
+            //insert to DB
+            console.log(hash)
+            const sql = 'INSERT INTO user (username, password, first_name, last_name, gender) VALUE (?, ?, ?, ?, ?)';
+
+            con.query(sql, [username, password, first_name, last_name, gender], function (err, result) {
+               
+                if (err) {
+                    console.log(err);
+                    res.status(500).send("Database server error.");
+                } else {
+                    if (result.affectedRows == 1) {
+                        res.send("New user has been added.");
+                    } else {
+                        res.status(501).send("Error while adding new user.");
+                    }
+                }
+            });
+        }
+    })
+
+});
+
+//---- get owner ----
+app.get("/user", (req, res) => {
+    const sql = "SELECT username, password FROM user";
+    con.query(sql, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Database server error");
+        } else {
+            res.json(result);
         }
     });
 });
