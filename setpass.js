c10113c9710ede21224a208faa60751e6d6b3057
Thiti Subsuksamran <thitiboll57@gmail.com>
2020-12-21 12:36:34 +0700

    code conplete

--- setpass.js
+++ setpass.js
@@ -1,57 +1,46 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const bcrypt = require("bcrypt");
 // ===== mysql =====
 const mysql = require("mysql");
 const config = require("./dbConfig.js");
+const { password } = require("./dbConfig.js");
 const con = mysql.createConnection(config);
 // Middleware
-app.use(express.static(path.join(__dirname, "css2")))  
-app.use(express.static(path.join(__dirname, "js"))) 
-app.use(express.static(path.join(__dirname, "images")))     
-                                        
+app.use(express.static(path.join(__dirname, "css2")))
+app.use(express.static(path.join(__dirname, "js")))
+app.use(express.static(path.join(__dirname, "images")))
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // ====== Page route ======
 // ====== Root service ======
-app.get("/Changepass" ,function(req,res){
+app.get("/Changepass", function (req, res) {
     res.sendFile(path.join(__dirname, "./changepass.html"));
 });
-app.get("/MyPurchase" ,function(req,res){
+app.get("/MyPurchase", function (req, res) {
     res.sendFile(path.join(__dirname, "./MyPurchase.html"));
 });
- 
-app.get("/MyPurchaseStatus" ,function(req,res){
+
+app.get("/MyPurchaseStatus", function (req, res) {
     res.sendFile(path.join(__dirname, "./MyPurchaseStatus.html"));
 });
-// ====== Other route ======
-app.post("/login",function(req,res){
-    const {username,password} = req.body;
-    const sql = `SELECT username, role FROM user WHERE username ='${username}' AND password='${password}'`;
-    con.query(sql, function(err,result,field){
-        if(err){
+// ====== Change Password ======
+app.post("/changepass", function (req, res) {
+
+    const oldpass = req.body.oldpass;
+    const newpass = req.body.newpass;
+
+    const sql = "UPDATE user SET password=? WHERE password=?";
+    con.query(sql, [ newpass,oldpass], function (err, result, fields) {
+        if (err) {
             console.log(err);
-        res.status(500).send("Database server error");
+            res.status(500).end("Server error");
         }
-        else{
-            if(result.length == 0 ){
-                res.status(400).send("Wrong username or password");
-            }
-            else{
-                // correct login
-                // res.send("welcome");
-                if(result[0].role == 1){
-                    res.send("admin");
-                }
-                else {
-                    res.send("user");
-                }
-            }
-        }
-        
     });
 });
+
 
 
 
@@ -59,4 +48,4 @@ app.post("/login",function(req,res){
 const port = 3000;
 app.listen(port, function () {
     console.log("Server is ready at " + port);
-});
+}); 
