5a9d5af53484e0b504efd5a07b5875d73e5d0314
Thiti Subsuksamran <thitiboll57@gmail.com>
2020-12-22 20:34:36 +0700

    findshopper image

--- setpass.js
+++ setpass.js
@@ -8,24 +8,64 @@ const config = require("./dbConfig.js");
 const { password } = require("./dbConfig.js");
 const con = mysql.createConnection(config);
 // Middleware
+app.use(express.static(path.join(__dirname, "css")))
 app.use(express.static(path.join(__dirname, "css2")))
 app.use(express.static(path.join(__dirname, "js")))
 app.use(express.static(path.join(__dirname, "images")))
-
+app.use(express.static(path.join(__dirname, "imagefindsp")))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // ====== Page route ======
-// ====== Root service ======
-app.get("/Changepass", function (req, res) {
-    res.sendFile(path.join(__dirname, "./changepass.html"));
-});
-app.get("/MyPurchase", function (req, res) {
-    res.sendFile(path.join(__dirname, "./MyPurchase.html"));
+
+app.get("/findshopper", function (req, res) {
+    res.sendFile(path.join(__dirname, "./findshopper.html"));
 });
 
-app.get("/MyPurchaseStatus", function (req, res) {
-    res.sendFile(path.join(__dirname, "./MyPurchaseStatus.html"));
+app.get("/changepass", function (req, res) {
+    res.sendFile(path.join(__dirname, "./changepass.html"));
 });
+app.get("/mypurchase", function (req, res) {
+    res.sendFile(path.join(__dirname, "./mypurchase.html"));
+});
+
+app.get("/mypurchaseStatus", function (req, res) {
+    res.sendFile(path.join(__dirname, "./mypurchasestatus.html"));
+});
+
+// ====== Login ======
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const sql = "SELECT username FROM user WHERE USERNAME=? AND PASSWORD=?";
+
+    con.query(sql, [username, password], function (err, result, fields) {
+        if (err) {
+            console.log(err);
+            res.status(500).end("Server error");
+            return;
+        }
+
+        // result is an array of records from database
+        const numrows = result.length;
+        //if no data
+        if (numrows != 1) {
+            res.status(401).end("Wrong username or password");
+        }
+        else {
+            if (result[0].username == 'Orawan021') {
+                res.send("/");
+                console.log("Success")
+            }
+            else {
+                res.send("/");
+            }
+        }
+    });
+});
+
+
+
+
 // ====== Change Password ======
 app.post("/changepass", function (req, res) {
 
